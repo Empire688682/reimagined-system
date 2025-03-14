de3ba204badc889731be449c0cb1f6a135f1aecd
Empire688682 <asehindej@gmail.com>
2025-03-14 19:24:01 +0100

    navbar

--- src/app/component/Navbar/Navbar.jsx
+++ src/app/component/Navbar/Navbar.jsx
@@ -75,27 +75,52 @@ const Navbar = () => {
             {/* Mobile Navbar */}
             <div className='flex md:hidden justify-between items-center'>
                 {/* Logo */}
-                <Image src="/colored-ayinla-logo.png" alt="logo" width={100} height={100} />
+                <Image src="/colored-ayinla-logo.png" alt="logo" width={80} height={80} />
 
                 {/* Mobile Menu Toggle Button */}
                 <div onClick={() => setIsOpen(!isOpen)}>
-                    <IoMenuOutline className='text-6xl text-white cursor-pointer' />
+                    <IoMenuOutline className='text-4xl text-white cursor-pointer' />
                 </div>
 
                 {/* Mobile Menu Content */}
                 {
                     isOpen && (
                         <div className='absolute top-16 left-0 py-4 w-full h-[80vh] bg-blue-600 flex flex-col justify-center items-center z-10'>
-                            <ul className='flex flex-col gap-5 justify-center items-center'>
+                            {/* Navigation Links */}
+                            <ul className='flex flex-col gap-8'>
                                 <li className='text-white cursor-pointer text-[20px]'>Home</li>
-                                <li className='flex items-center text-white cursor-pointer'>
+
+                                {/* Property Listing with Dropdown */}
+                                <li className='flex relative items-center text-white cursor-pointer' onClick={() => handleSubMenu(1)}>
                                     Property Listing
-                                    <IoIosArrowDown className='ml-1 text-white text-[20px]' />
+                                    <IoIosArrowDown className={`ml-1 text-white text-[20px] transition-transform ${openSubMenu[1] ? 'rotate-[180deg]' : ''}`} />
+                                    {
+                                        openSubMenu[1] && (
+                                            <div className='absolute top-[100%] w-[160px] rounded-sm p-3 flex flex-col gap-3 left-20 bg-white text-black'>
+                                                <span className="text-xs cursor-pointer font-semibold">List Your Property</span>
+                                                <hr className="text-gray-300" />
+                                                <span className="text-xs cursor-pointer font-semibold">Listing Request Status</span>
+                                            </div>
+                                        )
+                                    }
                                 </li>
-                                <li className='flex items-center text-white cursor-pointer'>
+
+                                {/* Booking with Dropdown */}
+                                <li className='flex relative items-center text-white cursor-pointer' onClick={() => handleSubMenu(2)}>
                                     Booking
-                                    <IoIosArrowDown className='ml-1 text-white text-[20px]' />
+                                    <IoIosArrowDown className={`ml-1 text-white text-[20px] transition-transform ${openSubMenu[2] ? 'rotate-[180deg]' : ''}`} />
+                                    {
+                                        openSubMenu[2] && (
+                                            <div className='absolute top-[100%] w-[160px] rounded-sm p-3 flex flex-col gap-3 left-[-20px] bg-white text-black'>
+                                                <span className="text-xs cursor-pointer font-semibold">Book a Space Now</span>
+                                                <hr className="text-gray-300" />
+                                                <span className="text-xs cursor-pointer font-semibold">Booking Status</span>
+                                            </div>
+                                        )
+                                    }
                                 </li>
+
+                                {/* Additional Links */}
                                 <li className='text-white cursor-pointer'>About Us</li>
                                 <li className='text-white cursor-pointer'>FAQ'S</li>
                                 <li className='text-white cursor-pointer'>Contact Information</li>
