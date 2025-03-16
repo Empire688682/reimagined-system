a3cafa4fce32b6b12efaeadff6bb91f0cc87f0d6
Empire688682 <asehindej@gmail.com>
2025-03-16 07:11:08 +0100

    booking, navba

--- src/app/component/Navbar/Navbar.jsx
+++ src/app/component/Navbar/Navbar.jsx
@@ -1,9 +1,10 @@
 "use client"
 import React, { useState } from 'react';
 import { usePathname } from 'next/navigation';
-import Image from "next/image"; 
+import Image from "next/image";
 import { IoIosArrowDown } from "react-icons/io";
 import { IoMenuOutline } from "react-icons/io5";
+import Link from 'next/link';
 
 const Navbar = () => {
     const [isOpen, setIsOpen] = useState(false);
@@ -25,21 +26,36 @@ const Navbar = () => {
         <nav className={`pl-3 pr-3 md:pl-6 md:pr-6 md:py-3 py-2 absolute top-0 left-0 right-0 z-10 border-b border-gray-400 transition-all duration-300 ${navbarBg}`}>
             <div className='md:flex hidden justify-between items-center'>
                 {/* Logo */}
-                <div className="relative w-[100px] h-[50px] cursor-pointer">
+                <Link href="/" className="relative w-[100px] h-[50px] cursor-pointer">
                     <Image src={logo} alt="logo" fill className="object-contain" />
-                </div>
+                </Link>
 
                 {/* Nav Links */}
                 <ul className='flex gap-8'>
                     <li className='text-white cursor-pointer'>Home</li>
                     <li className='flex relative items-center text-white cursor-pointer' onClick={() => handleSubMenu(1)}>
-                        Property Listing
+                        Property List
                         <IoIosArrowDown className={`ml-1 text-white text-[20px] transition-transform ${openSubMenu[1] ? 'rotate-[180deg]' : ''}`} />
                         {openSubMenu[1] && (
                             <div className='absolute top-[100%] w-[160px] rounded-sm p-3 flex flex-col gap-3 left-0 bg-white text-black'>
+                                <Link href="/properties" passHref className="text-xs cursor-pointer font-semibold" onClick={() => setIsOpen(false)}>
+                                    Properties
+                                </Link>
+                                <hr className="text-gray-300" />
                                 <span className="text-xs cursor-pointer font-semibold">List Your Property</span>
                                 <hr className="text-gray-300" />
                                 <span className="text-xs cursor-pointer font-semibold">Listing Request Status</span>
+                            </div>
+                        )}
+                    </li>
+                    <li className='flex relative items-center text-white cursor-pointer' onClick={() => handleSubMenu(2)}>
+                        Booking List
+                        <IoIosArrowDown className={`ml-1 text-white text-[20px] transition-transform ${openSubMenu[2] ? 'rotate-[180deg]' : ''}`} />
+                        {openSubMenu[2] && (
+                            <div className='absolute top-[100%] w-[160px] rounded-sm p-3 flex flex-col gap-3 left-0 bg-white text-black'>
+                                <Link href="/booking" className="text-xs cursor-pointer font-semibold" onClick={()=>setIsOpen(false)}>Booking</Link>
+                                <hr className="text-gray-300" />
+                                <span className="text-xs cursor-pointer font-semibold">Booking Status</span>
                             </div>
                         )}
                     </li>
@@ -57,16 +73,42 @@ const Navbar = () => {
 
             {/* Mobile Navbar */}
             <div className='flex md:hidden justify-between items-center'>
-                <div className="relative w-[80px] h-[40px]">
+                <Link href="/" className="relative w-[100px] h-[50px] cursor-pointer">
                     <Image src={logo} alt="logo" fill className="object-contain" />
-                </div>
+                </Link>
                 <div onClick={() => setIsOpen(!isOpen)}>
                     <IoMenuOutline className='text-4xl text-white cursor-pointer' />
                 </div>
                 {isOpen && (
-                    <div className='absolute top-16 left-0 py-4 w-full h-[80vh] bg-blue-600 flex flex-col justify-center items-center z-10'>
+                    <div className='absolute top-14 left-0 py-4 w-full h-[80vh] bg-blue-600 flex flex-col justify-center items-center z-10'>
                         <ul className='flex flex-col gap-8'>
                             <li className='text-white cursor-pointer'>Home</li>
+                            <li className='flex relative items-center text-white cursor-pointer' onClick={() => handleSubMenu(1)}>
+                                Property List
+                                <IoIosArrowDown className={`ml-1 text-white text-[20px] transition-transform ${openSubMenu[1] ? 'rotate-[180deg]' : ''}`} />
+                                {openSubMenu[1] && (
+                                    <div className='absolute top-[100%] w-[160px] rounded-sm p-3 flex flex-col gap-3 left-0 bg-white text-black'>
+                                        <Link href="/properties" passHref className="text-xs cursor-pointer font-semibold">
+                                            Properties
+                                        </Link>
+                                        <hr className="text-gray-300" />
+                                        <span className="text-xs cursor-pointer font-semibold">List Your Property</span>
+                                        <hr className="text-gray-300" />
+                                        <span className="text-xs cursor-pointer font-semibold">Listing Request Status</span>
+                                    </div>
+                                )}
+                            </li>
+                            <li className='flex relative items-center text-white cursor-pointer' onClick={() => handleSubMenu(2)}>
+                                Booking List
+                                <IoIosArrowDown className={`ml-1 text-white text-[20px] transition-transform ${openSubMenu[2] ? 'rotate-[180deg]' : ''}`} />
+                                {openSubMenu[2] && (
+                                    <div className='absolute top-[100%] w-[160px] rounded-sm p-3 flex flex-col gap-3 left-0 bg-white text-black'>
+                                        <Link href="/booking" className="text-xs cursor-pointer font-semibold" onClick={()=>setIsOpen(false)}>Booking</Link>
+                                        <hr className="text-gray-300" />
+                                        <span className="text-xs cursor-pointer font-semibold">Booking Status</span>
+                                    </div>
+                                )}
+                            </li>
                             <li className='text-white cursor-pointer'>About Us</li>
                             <li className='text-white cursor-pointer'>FAQ'S</li>
                             <li className='text-white cursor-pointer'>Contact Information</li>
