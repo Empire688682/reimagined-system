27adf5a558fa76e8b7cf351f60d8827384469a1c
Empire688682 <asehindej@gmail.com>
2025-03-29 05:45:45 +0100

    video

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,12 +1,25 @@
-import React from 'react';
+"use client";
+import React, { useEffect, useRef } from 'react';
 import Hero from '@/component/Hero/Hero';
 import Services from '@/component/Services/Services';
 import Footer from '@/component/Footer/Footer';
 import Properties from '@/component/Properties/Properties';
 import Features from '@/component/Features/Features';
 import Journey from '@/component/Journey/Journey';
+import { FaRegCirclePlay } from "react-icons/fa6";
+import { FaPauseCircle } from "react-icons/fa";
 
 const Page = () => {
+  const [isPlaying, setIsPlaying] = React.useState(false);
+  const videoRef = useRef();
+
+  useEffect(() => {
+    if (videoRef.current) {
+      videoRef.current.addEventListener('playing', () => setIsPlaying(true));
+      videoRef.current.addEventListener('pause', () => setIsPlaying(false));
+    }
+  }, [isPlaying]);
+
   return (
     <div className="relative w-full bg-[#F1F9FF]">
       <Hero />
@@ -14,7 +27,8 @@ const Page = () => {
       <Properties />
       {/* Background Video Section */}
       <div className="w-full relative overflow-hidden">
-        <video 
+        <video
+          ref={videoRef}
           className="w-full h-[70vh] object-cover" 
           autoPlay 
           loop 
@@ -22,8 +36,20 @@ const Page = () => {
           playsInline
           src="/building-video-compressed.mp4"
         ></video>
+        <div>
+          {isPlaying ? (
+            <FaPauseCircle
+              className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white text-5xl cursor-pointer"
+              onClick={() => videoRef.current.pause()}
+            />
+          ) : (
+            <FaRegCirclePlay
+              className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white text-5xl cursor-pointer"
+              onClick={() => videoRef.current.play()}
+            />
+          )}
+        </div>
       </div>
-
       <Features />
       <Journey />
       <Footer />
